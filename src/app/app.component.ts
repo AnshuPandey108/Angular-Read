2f6a22ac7aad59cc7a1b80fff52eb3dbce4b5a69
Anshu Pandey <anshu.pandey@avista.com>
2025-06-13 13:03:19 +0530

    switch case in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { EventComponent } from './event/event.component';
 import { InputComponent } from './input/input.component';
 import { StyleComponent } from './style/style.component';
 import { IfElseComponent } from './if-else/if-else.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent, EventComponent, InputComponent, StyleComponent, IfElseComponent],
+  imports: [RouterOutlet, CounterComponent, EventComponent, InputComponent, StyleComponent, IfElseComponent, SwitchComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
